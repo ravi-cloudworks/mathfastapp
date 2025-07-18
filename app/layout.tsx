f441b14f8b416413c63cc8a4a37a9b7ff20e4073
Ravi <ravipal@tvtail.com>
2025-07-19 00:59:24 +0530

    update math fast app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'MathFast App',
+  description: 'MathFast App',
+  generator: 'ravishankar',
 }
 
 export default function RootLayout({
